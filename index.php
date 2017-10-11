17148395cffdfce9fb854704ce88e3b7fab7253e
damiandpx <32001554+damiandpx@users.noreply.github.com>
2017-10-11 14:36:37 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>h1 tags</title>
-</head>
-<body>
-<h1><?php echo 'Hola Profesora Rosa Isela' ?></h1>
-<p>Mi primera practica Jalando repositorio de Github</p>
-</body>
-</html>
